7b200c761d47c016ce240f7fd9d8ae30731eb4fd
Alexandra Calin <72324878+alecalin366@users.noreply.github.com>
2021-03-17 03:36:29 +0200

    o parte din navBar + profile

--- app/src/main/java/com/example/food/Home/MainActivity.java
+++ app/src/main/java/com/example/food/Home/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.food.Home;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,14 +10,21 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.food.Likes.LikesFragment;
 import com.example.food.Login.LoginActivity;
+import com.example.food.Profile.ProfileFragment;
 import com.example.food.R;
+import com.example.food.Search.SearchFragment;
+import com.example.food.ShopActivity.ShopFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static final int ACTIVITY_NUM = 0; //pentru a stii mai jos care dintre item sa le highligh-uiasca cand dam click pe ele
+    private ChipNavigationBar chipNavigationBar;
+    private Fragment fragment = null;
 
     //firebase
     private FirebaseAuth mAuth;
@@ -28,6 +36,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupFirebaseAuth();
+
+        chipNavigationBar = findViewById(R.id.navBar);
+
+        chipNavigationBar.setItemSelected(R.id.ic_house, true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                switch (i) {
+                    case R.id.ic_house:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.ic_search:
+                        fragment = new SearchFragment();
+                        break;
+                    case R.id.ic_circle:
+                        fragment = new ShopFragment();
+                        break;
+                    case R.id.ic_alert:
+                        fragment = new LikesFragment();
+                        break;
+                    case R.id.ic_android:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+
+                if (fragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                }
+            }
+        });
+
 
         //mAuth.signOut();
     }
