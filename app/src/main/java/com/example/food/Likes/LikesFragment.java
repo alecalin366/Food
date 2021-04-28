c6eff9913efeb8b4e32951b0e08199e28a7140e5
Alexandra Calin <72324878+alecalin366@users.noreply.github.com>
2021-04-28 18:30:09 +0300

    Add new recipe layout

--- app/src/main/java/com/example/food/Likes/LikesFragment.java
+++ app/src/main/java/com/example/food/Likes/LikesFragment.java
@@ -61,6 +61,6 @@ public class LikesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_likes, container, false);
+        return inflater.inflate(R.layout.add_recipe_layout, container, false);
     }
 }
