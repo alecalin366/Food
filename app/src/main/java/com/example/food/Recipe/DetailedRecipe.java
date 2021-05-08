34640160f35428bac8bae82d467c5194ce68bbbf
Alexandra Calin <72324878+alecalin366@users.noreply.github.com>
2021-05-09 01:32:46 +0300

    button

--- app/src/main/java/com/example/food/Recipe/DetailedRecipe.java
+++ app/src/main/java/com/example/food/Recipe/DetailedRecipe.java
@@ -2,6 +2,7 @@ package com.example.food.Recipe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -37,7 +38,17 @@ public class DetailedRecipe extends AppCompatActivity {
 
         FindViews();
         SetViewsFields();
+        setupBackButton();
 
+    }
+
+    public void setupBackButton(){
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     private void FindViews()
